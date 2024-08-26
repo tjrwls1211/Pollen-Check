b31826fb8cd8207de3d18eabc9be83a25b359346
tjrwls1211 <seokjin6635@gmail.com>
2024-08-26 22:08:00 +0900

    서비스 키 내용 제거, 사용자가 env 파일에 서비스 키 삽입해서 사용할 수 있도록 수정

--- components/FetchOakTreePollen.js
+++ components/FetchOakTreePollen.js
@@ -1,5 +1,6 @@
 const FetchOakTreePollen = (cityNumber, date) => {
-  return fetch(`https://apis.data.go.kr/1360000/HealthWthrIdxServiceV3/getOakPollenRiskIdxV3?serviceKey=PR2F0BLMetFiBoKaFaD9Tz7tCfK783UkoEcTBiE2Hp8oyrtZ2pT3nBwONkpX5hJ81rv83L6%2BfEUvTOpCclVHwA%3D%3D&numOfRows=10&pageNo=1&dataType=JSON&areaNo=${cityNumber}&time=${date}`) //참나무 꽃가루 api 호출 ** 안되면 api 키 인코딩 -> 디코딩으로 바꿔서 해보기
+  const serviceKey = process.env.REACT_APP_SERVICE_KEY
+  return fetch(`https://apis.data.go.kr/1360000/HealthWthrIdxServiceV3/getOakPollenRiskIdxV3?serviceKey=${serviceKey}&numOfRows=10&pageNo=1&dataType=JSON&areaNo=${cityNumber}&time=${date}`) //참나무 꽃가루 api 호출 ** 안되면 api 키 인코딩 -> 디코딩으로 바꿔서 해보기
     .then(response => response.json())
     .then(responseData => {
       const todayCount = responseData.response.body.items.item[0].today; // 오늘의 꽃가루 수치 추출
--- components/FetchPineTreePollen.js
+++ components/FetchPineTreePollen.js
@@ -1,5 +1,6 @@
 const FetchPineTreePollen = (cityNumber, date) => {
-  return fetch(`https://apis.data.go.kr/1360000/HealthWthrIdxServiceV3/getPinePollenRiskIdxV3?serviceKey=PR2F0BLMetFiBoKaFaD9Tz7tCfK783UkoEcTBiE2Hp8oyrtZ2pT3nBwONkpX5hJ81rv83L6%2BfEUvTOpCclVHwA%3D%3D&numOfRows=10&pageNo=1&dataType=JSON&areaNo=${cityNumber}&time=${date}`) //소나무 꽃가루 api 호출
+  const serviceKey = process.env.REACT_APP_SERVICE_KEY;
+  return fetch(`https://apis.data.go.kr/1360000/HealthWthrIdxServiceV3/getPinePollenRiskIdxV3?serviceKey=${serviceKey}&numOfRows=10&pageNo=1&dataType=JSON&areaNo=${cityNumber}&time=${date}`) //소나무 꽃가루 api 호출
     .then(response => response.json())
     .then(responseData => {
       const todayCount = responseData.response.body.items.item[0].today; // 오늘의 꽃가루 수치 추출
